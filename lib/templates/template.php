712e7e435046d9acd8d0d5aba3ce9b72c0f6148f
Marko Aleksic <psybaron@gmail.com>
2013-07-16 18:55:32 +0200

    dev2 with maximum

---
+++ lib/templates/template.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MAXIMUM - All in One Theme</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/onepage.css" rel="stylesheet">
+    <link href="assets/css/flexslider.css" rel="stylesheet">
+
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+
+    <!-- Google Fonts call. Font Used Open Sans & Open Sans Condensed -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+
+    <!-- JQuery script -->
+    <script type="text/javascript" src="assets/js/jquery.js"></script>
+    <!-- Le javascript
+    ================================================== -->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand cd-brand" href="home">CARNIA DESIGN STUDIO</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+                <li class="active"><a href="#">About Us</a></li>
+                <li><a href="home#portfolio">Portfolio</a></li>
+                <li><a href="home#services">Services</a></li>
+                <li><a href="home#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <!-- Slider -->
+    <section id="slider">
+        <div class="flexslider">
+            <ul class="slides">
+                <!-- Slide -->
+                <li>
+                    <h1>WELCOME TO <br>CARNIA DESIGN STUDIO <br>
+                        <p>
+                            We design &amp; develop beautify website and web apps. <br>
+                            Our methods are contemporary; out works - timeless.
+                        </p>
+                    </h1>
+                    <img src="assets/img/onepage/pictures/slide-1.jpg" alt="">
+                </li>
+            </ul>
+        </div><!-- /flexslider -->
+    </section><!-- #slider -->
+
+
+    <!-- Portfolio Page -->
+    <article class="page portfolio" id="portfolio">
+        <div class="container">
+            <div class="page-title"><h1>Featured Work</h1></div>
+            <p><b>SEE SOME OF OUR BEST WORKS</b></p>
+
+            <div class="portfolio-items">
+                <div class="row">
+
+                    <!-- Portfolio Item -->
+                        <dl class="span4">
+                            <dt><img src="assets/img/onepage/pictures/portfolio-1.jpg" alt=""></dt>
+                            <dd>Portfolio 1</dd>
+                        </dl><!-- /span4 -->
+
+
+                    <!-- Portfolio Item -->
+                        <dl class="span4">
+                            <dt><img src="assets/img/onepage/pictures/portfolio-2.jpg"  alt=""></dt>
+                            <dd>Portfolio 2</dd>
+                        </dl><!-- /span4 -->
+
+                    <!-- Portfolio Item -->
+                        <dl class="span4">
+                            <dt><img src="assets/img/onepage/pictures/portfolio-3.jpg" alt=""></dt>
+                            <dd>Portfolio 3</dd>
+                        </dl><!-- /span4 -->
+
+                    <!-- Portfolio Item -->
+                        <dl class="span4">
+                            <dt><img src="assets/img/onepage/pictures/portfolio-4.jpg" alt=""></dt>
+                            <dd>Portfolio 4</dd>
+                        </dl><!-- /span4 -->
+
+                    <!-- Portfolio Item -->
+                        <dl class="span4">
+                            <dt><img src="assets/img/onepage/pictures/portfolio-5.jpg" alt=""></dt>
+                            <dd>Portfolio 5</dd>
+                        </dl><!-- /span4 -->
+
+                    <!-- Portfolio Item -->
+                        <dl class="span4">
+                            <dt><img src="assets/img/onepage/pictures/portfolio-6.jpg" alt=""></dt>
+                            <dd>Portfolio 6</dd>
+                        </dl>
+                </div><!-- /row -->
+            </div><!-- /portfolio-item -->
+
+        </div><!-- /container -->
+    </article>
+    <!-- /portfolio -->
+
+
+    <!-- Services Page -->
+    <article class="page even" id="services">
+
+        <div class="container">
+            <div class="page-title"><h1>What We Do</h1></div>
+            <p><b>THIS IS WHAT WE DO</b></p>
+        </div><!-- container -->
+
+        <div class="widget-area">
+            <div class="container">
+                <div class="row-fluid">
+                    <!-- Item -->
+                    <div class="widget widget-teaser width-33">
+                        <img src="assets/img/service01.png" alt="">
+                        <h3 class="widget-title">Service 1</h3>
+                        <div class="widget-content">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        </div><!-- /widget-content -->
+                    </div><!-- /widget -->
+                    <!-- Item -->
+                    <div class="widget widget-teaser width-33">
+                        <img src="assets/img/service02.png" alt="">
+                        <h3 class="widget-title">Service 2</h3>
+                        <div class="widget-content">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        </div><!-- /widget-content -->
+                    </div><!-- /widget -->
+                    <!-- Item -->
+                    <div class="widget widget-teaser width-33">
+                        <img src="assets/img/service03.png" alt="">
+                        <h3 class="widget-title">Service 3</h3>
+                        <div class="widget-content">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        </div><!-- /widget-content -->
+                    </div><!-- /widget -->
+                </div><!-- /row -->
+            </div><!-- /container -->
+        </div><!-- /widget -->
+
+    </article>
+    <!-- #services -->
+
+
+    <!-- Team Page -->
+    <article class="page staff">
+        <div class="container">
+            <div class="page-title"><h1>An Awesome Team</h1></div>
+            <p><b>WE ARE A SMALL BUT DEDICATED TEAM</b></p>
+
+            <div class="staff-items">
+                <div class="row">
+                    <!-- Item -->
+                    <dl class="span4">
+                        <dt><img src="assets/img/team/team01.jpg" alt=""></dt>
+                        <dd>
+                            <h3>Lisa Lynn</h3>
+                            <p>- Developer -</p>
+                            <ul class="social-profiles">
+                                <li><a href="#" title="Dribbble"><img src="assets/img/onepage/i-dribbble.png" alt="Dribbble"></a></li>
+                                <li><a href="#" title="Forrst"><img src="assets/img/onepage/i-forrst.png" alt="Forrst"></a></li>
+                                <li><a href="#" title="Twitter"><img src="assets/img/onepage/i-twitter.png" alt="Twitter"></a></li>
+                            </ul>
+                        </dd>
+                    </dl><!-- /span4 -->
+
+                    <!-- Item -->
+                    <dl class="span4">
+                        <dt><img src="assets/img/team/team02.jpg" alt=""></dt>
+                        <dd>
+                            <h3>Marcel Newman</h3>
+                            <p>- Designer -</p>
+                            <ul class="social-profiles">
+                                <li><a href="#" title="Dribbble"><img src="assets/img/onepage/i-dribbble.png" alt="Dribbble"></a></li>
+                                <li><a href="#" title="Forrst"><img src="assets/img/onepage/i-forrst.png" alt="Forrst"></a></li>
+                                <li><a href="#" title="Twitter"><img src="assets/img/onepage/i-twitter.png" alt="Twitter"></a></li>
+                            </ul>
+                        </dd>
+                    </dl><!-- /span4 -->
+
+                    <!-- Item -->
+                    <dl class="span4">
+                        <dt><img src="assets/img/team/team03.jpg" alt=""></dt>
+                        <dd>
+                            <h3>Carol Lorca</h3>
+                            <p>- Marketing -</p>
+                            <ul class="social-profiles">
+                                <li><a href="#" title="Dribbble"><img src="assets/img/onepage/i-dribbble.png" alt="Dribbble"></a></li>
+                                <li><a href="#" title="Forrst"><img src="assets/img/onepage/i-forrst.png" alt="Forrst"></a></li>
+                                <li><a href="#" title="Twitter"><img src="assets/img/onepage/i-twitter.png" alt="Twitter"></a></li>
+                            </ul>
+                        </dd>
+                    </dl><!-- /span4 -->
+                </div><!-- /row -->
+            </div><!-- / -->
+        </div><!-- /container -->
+    </article>
+    <!-- #the-team -->
+
+
+    <!-- Connect Page -->
+    <article class="page portfolio even">
+        <div class="container">
+            <div class="row">
+                <div class="span12 connect">
+                    <div class="page-title"><h1>Connect With Us</h1></div>
+                    <p>Connect with us through social media and get updates and news.</p>
+                        <a href="#"><img src="assets/img/onepage/c-dribbble.png" alt="Dribbble"></a>
+                        <a href="#"><img src="assets/img/onepage/c-forrst.png" alt="Forrst"></a>
+                        <a href="#"><img src="assets/img/onepage/c-twitter.png" alt="Twitter"></a>
+                        <a href="#"><img src="assets/img/onepage/c-facebook.png" alt="Facebook"></a>
+                        <a href="#"><img src="assets/img/onepage/c-pinterest.png" alt="Pinterest"></a>
+                        <a href="#"><img src="assets/img/onepage/c-linkedin.png" alt="LinkedIn"></a>
+                </div><!-- /span12 -->
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </article>
+    <!-- #connect -->
+
+
+    <!-- Clients Page -->
+    <article class="page staff">
+        <div class="container">
+            <div class="page-title"><h1>Selected Clients</h1></div>
+            <p><b>BRANDS WE HAVE WORKED FOR</b></p>
+                <div class="client-items">
+                    <div class="row">
+                        <!-- Item -->
+                        <div class="span4">
+                            <img src="assets/img/onepage/pictures/client-1.png" alt="">
+                        </div><!-- /span4 -->
+                        <!-- Item -->
+                        <div class="span4">
+                            <img src="assets/img/onepage/pictures/client-2.png" alt="">
+                        </div><!-- /span4 -->
+                        <!-- Item -->
+                        <div class="span4">
+                            <img src="assets/img/onepage/pictures/client-3.png" alt="">
+                        </div><!-- /span4 -->
+                    </div>
+                </div><!-- /client-items -->
+        </div><!-- /container -->
+    </article><!-- /article -->
+    <!-- #clients -->
+
+    <!-- Contact Page -->
+    <article class="page portfolio even" id="contact">
+
+    <!-- Map -->
+        <div class="map"><iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?ie=UTF8&amp;q=las+tablas,+madrid&amp;fb=1&amp;hq=las+tablas,&amp;hnear=0xd42287d383766c9:0xad469cc0ed50997d,Madrid,+Espa%C3%B1a&amp;cid=0,0,15011046097507982567&amp;t=m&amp;iwloc=A&amp;ll=40.421889,-3.713523&amp;spn=0.006295,0.006295&amp;output=embed"></iframe></div>
+        <br>
+        <div class="container">
+            <div class="page-title"><h1>Contact Us</h1></div>
+            <p><b>WE ARE ACCEPTING NEW PROJECTS</b></p>
+
+            <div class="address">
+                <div class="row">
+                    <div class="span3"></div>
+                    <div class="span6">
+                        <form action="#get-in-touch" method="POST" id="contact">
+                            <input type="text" id="contactname" name="contactname" placeholder="Name">
+                            <input type="text" id="email" name="email" placeholder="Email">
+                            <div class="textarea-container"><textarea id="message" name="message" placeholder="Message"></textarea></div>
+                            <input type="submit" id="submit" name="submit" value="Send">
+                        </form>
+                    </div><!-- /span6 -->
+                    <div class="span3"></div>
+                </div><!-- /row -->
+            </div><!-- /address -->
+        </div><!-- /container -->
+
+    </article>
+    <!-- #get-in-touch -->
+
+
+    <!-- Footer -->
+    <div id="twitterwrap">
+        <img src="assets/img/twitter.png" alt="">
+        <br>
+        <br>
+        <div id="jstwitter" class="clearfix">
+            <ul id="twitter_update_list"></ul>
+        </div>
+        <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script><!-- Script Needed to load the Tweets -->
+        <script type="text/javascript" src="http://api.twitter.com/1/statuses/user_timeline/basicoh_.json?callback=twitterCallback2&count=1"></script>
+    </div>
+    <div id="footerwrap">
+    <em>CARNIA DESIGN STUDIO</em>
+    <br>
+    <br>
+    <p>INDUSTRIAL ZONE 2.</p>
+    <p>1300, KUMANOVO</p>
+    <br>
+    <p><a href="#">GET DIRECTIONS</a></p>
+    <br>
+    <div class="social_icons">
+        <a href="#"><img src="assets/img/social/white/dribbble.png" width="30" height="30" alt="Dribbble"></a>
+        <a href="#"><img src="assets/img/social/white/facebook.png" width="30" height="30" alt="Facebook"></a>
+        <a href="#"><img src="assets/img/social/white/gplus.png" width="30" height="30" alt="GooglePlus"></a>
+        <a href="#"><img src="assets/img/social/white/instagram.png" width="30" height="30" alt="Instagram"></a>
+        <a href="#"><img src="assets/img/social/white/linkedin.png" width="30" height="30" alt="LinkedIn"></a>
+        <a href="#"><img src="assets/img/social/white/pinterest.png" width="30" height="30" alt="Pinterest"></a>
+        <a href="#"><img src="assets/img/social/white/twitter.png" width="30" height="30" alt="Twitter"></a>
+    </div><!-- End of social icons -->
+
+
+    </div>
+
+    <!-- Le javascript
+    ================================================== -->
+    <script type="text/javascript" src="assets/js/jquery.flexslider.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    <script type="text/javascript" src="assets/js/onepage/js/main.js"></script>
+  </body>
+</html>
